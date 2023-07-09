223f56dad4828cb9b907106df3b29a527f3df7a3
Srinivas Muthu <smuthu@linkedin.com>
2023-07-09 14:48:31 -0700

    Added proper dictionary, scrambling word corretly

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
+function main() {
+    word = pick_word()
+    jumbled_word = scramble(word)
+}
+
+function pick_word() {
+    // Need to read resources/words_alpha.txt to pick one with L == 9
+    return "tenacious";
+}
+
+function scramble(word) {
+    strarray = word.split('');
+    var i,j,k
+    for (i = 0; i < strarray.length; i++) {
+      j = Math.floor(Math.random() * i)
+      k = strarray[i]
+      strarray[i] = strarray[j]
+      strarray[j] = k
+    }
+    word = strarray.join('');
+    return word;
+}
+
 function search(ele) {
-    if(event.key === 'Enter') {
-        alert(ele.value);        
+    if(event.key === 'Enter' && ele.value === 'clawing') {
+        alert("Congrats, you got the pangram!");
+        ele.value = '';
     }
 }
+
+main()
