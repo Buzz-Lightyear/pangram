fc7f882020bc6c2dc8a1ceeca1d349cc27547d96
Srinivas Muthu <smuthu@linkedin.com>
2023-07-09 14:57:15 -0700

    Letters correctly populating

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
-function main() {
+function start_game() {
     word = pick_word()
     jumbled_word = scramble(word)
+    populate_grid(jumbled_word);
+}
+
+function populate_grid(word) {
+    window.onload = function () {
+        document.getElementById("letter1").innerHTML = word[0];
+        document.getElementById("letter2").innerHTML = word[1];
+        document.getElementById("letter3").innerHTML = word[2];
+        document.getElementById("letter4").innerHTML = word[3];
+        document.getElementById("letter5").innerHTML = word[4];
+        document.getElementById("letter6").innerHTML = word[5];
+        document.getElementById("letter7").innerHTML = word[6];
+        document.getElementById("letter8").innerHTML = word[7];
+        document.getElementById("letter9").innerHTML = word[8];
+    };
 }
 
 function pick_word() {
@@ -21,11 +36,11 @@ function scramble(word) {
     return word;
 }
 
-function search(ele) {
+function check_answer(ele) {
     if(event.key === 'Enter' && ele.value === 'clawing') {
         alert("Congrats, you got the pangram!");
         ele.value = '';
     }
 }
 
-main()
+start_game()
